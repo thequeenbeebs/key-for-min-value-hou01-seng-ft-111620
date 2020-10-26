394404ac7ffc0d6b68e9ae5b65102a7bfcac1eef
Blaire Baker <bakerblaire@gmail.com>
2020-10-26 18:50:51 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   min_value = nil
   min_key = nil
   name_hash.each do |key, value|
-    if min_value > value
+    if min_value == nil || min_value > value
       min_key = key
       min_value = value
     end
